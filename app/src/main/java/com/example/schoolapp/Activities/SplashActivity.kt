ad51135cf0adb0bb2e20e9bcaf6d8e07d368bc00
mitrikM <marek.mitrik11@gmail.com>
2024-01-29 19:13:29 +0100

    splashscreen implementation

---
+++ app/src/main/java/com/example/schoolapp/Activities/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.schoolapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashDisplayLength = 1000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashDisplayLength)
+    }
+}
